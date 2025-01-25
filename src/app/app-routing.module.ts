269de9223bdb8fcae1faa616446cc22858cd6f16
migslahorra <migslahorra>
2025-01-25 08:48:07 +0800

    added changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostCreateComponent } from './post/post-create/post-create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'post-create', component: PostCreateComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
